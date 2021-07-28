20c35037dff4a3dae88d66f5d64b64a49f60ba0f
Drucetination <drucetination@yandex.ru>
2021-07-28 10:38:16 +0100

    start of development

--- string_splitter.py
+++ string_splitter.py
@@ -2,7 +2,12 @@ class TagManipulator():
     def parse_string(self, tags, regex=""):
         result = []
 
-        if len(tags) < 1:
+        if len(tags) < 1 or tags == ',':
             return result
+        else:
+            result = tags.split(',')
+            for res in result:
+                if res == '':
+                    result.remove(res)
 
         return result
--- test_string_splitter.py
+++ test_string_splitter.py
@@ -1,5 +1,6 @@
 import string_splitter
 
+
 def test_split_empty_string_result_empty_array():
     # arrange
     stringToSplit = ""
@@ -13,11 +14,27 @@ def test_split_empty_string_result_empty_array():
     # assert
     assert result == expResult
 
+
 def test_split_comma_empty_string_result_empty_array():
     # arrange
     stringToSplit = ","
 
     expResult = []
+    result = None
+    cut = string_splitter.TagManipulator()
+
+    # act
+    result = cut.parse_string(stringToSplit)
+
+    # assert
+    assert result == expResult
+
+
+def test_split_one_string_result_array_of_one():
+    # arrange
+    stringToSplit = "java"
+
+    expResult = ["java"]
     result = None
     cut = string_splitter.TagManipulator()
 
